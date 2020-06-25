6266a8ff6b79148bcd4b6871a9c4eb21ecd6f197
jwcelyo <jwcelyo@gmail.com>
2020-06-25 18:31:47 -0300

    Update sqlinstall.sh

--- sqlinstall.sh
+++ sqlinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+iplocal=$(hostname -I)
+echo "IP local do Servidor ${iplocal}"   
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)"
 sudo apt-get update
@@ -11,6 +13,7 @@ sudo apt-get install mssql-tools unixodbc-dev
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
-sudo /opt/mssql/bin/mssql-conf set network.ipaddress 127.0.0.1
+wget -O /var/opt/mssql/mssql.conf https://raw.githubusercontent.com/jwcelyo/dev/master/mssql.conf
+sudo /opt/mssql/bin/mssql-conf set network.ipaddress $iplocal
 sudo /opt/mssql/bin/mssql-conf set network.tcpport 3389
 reboot
