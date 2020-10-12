9f3b0b5e876e72931c467efe3cbb2fa84229a02e
jwcelyo <jwcelyo@gmail.com>
2020-10-12 00:23:11 -0300

    Add files via upload

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -2,10 +2,12 @@
 iplocal=$(hostname -I)
 echo "IP local do Servidor ${iplocal}"   
 sudo apt-get update
-sudo apt install mysql-server
+sudo apt install -y mysql-server
 sudo mysql_secure_installation
 systemctl status mysql.service
 service mysql stop
 sed -i "s/.*bind-address.*/bind-address = ${iplocal}/" /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql start
+echo "execute o script: GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'suasenha';"   
+sudo mysql
 
