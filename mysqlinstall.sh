95cfd72de7d84b7f4849f107e2a17afc18dff04c
jwcelyo <jwcelyo@gmail.com>
2020-10-12 00:12:50 -0300

    Add files via upload

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -6,6 +6,6 @@ sudo apt install mysql-server
 sudo mysql_secure_installation
 systemctl status mysql.service
 service mysql stop
-sed -i "s/.*bind-address.*/bind-address = ${iplocal}/" /etc/mysql/my.cnf
+sed -i "s/.*bind-address.*/bind-address = ${iplocal}/" /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql start
 
