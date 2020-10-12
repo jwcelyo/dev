11bd6f8506991ee39355be888ac044d0af2ee0e8
jwcelyo <jwcelyo@gmail.com>
2020-10-12 00:55:44 -0300

    Add files via upload

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -6,11 +6,12 @@ read varname
 echo A senha digitada foi $varname
 sudo apt-get update
 sudo apt install -y mysql-server
+sudo mysql_secure_installation
 systemctl status mysql.service
 service mysql stop
 sed -i "s/.*bind-address.*/bind-address = ${iplocal}/" /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql start
 echo "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '${varname}';"  
-sudo mysql -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '${varname}';" 
+sudo mysql -u root -p -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY '${varname}';" 
 
 
