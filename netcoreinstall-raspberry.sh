54d3e5ba1e016302f9b54826574b92e83b865c50
jwcelyo <jwcelyo@gmail.com>
2020-11-15 18:19:33 -0300

    Add files via upload

--- netcoreinstall-raspberry.sh
+++ netcoreinstall-raspberry.sh
@@ -1,6 +1,9 @@
-sudo apt-get install curl libunwind8 gettext
 wget https://download.visualstudio.microsoft.com/download/pr/e0d4bf96-e481-4c16-a2dc-69f002f2f892/ee8457bcbfae80bb4e76efe04f5bd3e2/dotnet-runtime-2.1.23-linux-arm.tar.gz
-mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-2.2.102-linux-arm.tar.gz -C $HOME/dotnet
-export DOTNET_ROOT=$HOME/dotnet 
-export PATH=$PATH:$HOME/dotnet
+mkdir -p /usr/share/dotnet && tar zxf dotnet-runtime-2.1.23-linux-arm.tar.gz -C /usr/share/dotnet
+ln -s /usr/share/dotnet /usr/bin/dotnet
+export DOTNET_ROOT=/usr/bin/dotnet
+export PATH=$PATH:/usr/bin/dotnet
 dotnet --info
+echo "export DOTNET_ROOT=/usr/bin/dotnet"
+echo "export PATH=$PATH:/usr/bin/dotnet"
+sudo nano .profile
